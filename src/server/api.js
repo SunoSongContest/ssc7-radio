8720efbf670ee7777921956219f5fb1ff7c5ed19
Marco <marcodz554@gmail.com>
2025-06-23 21:12:14 +0200

    Update api.js

--- src/server/api.js
+++ src/server/api.js
@@ -1,37 +1,17 @@
 const express = require("express");
 const path = require("path");
-const { google } = require("googleapis");
+const fs = require("fs").promises;
 const router = express.Router();
 
 require("dotenv").config();
 
-const googleAuth = new google.auth.GoogleAuth({
-  credentials: JSON.parse(process.env.GOOGLE_CREDENTIALS),
-  scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
-});
-
 router.get("/data", async (req, res) => {
   try {
-    const client = await googleAuth.getClient();
-    const sheets = google.sheets({ version: "v4", auth: client });
+    const filePath = path.join(__dirname, "data", "accepted_songs.json"));
 
-    const sheetData = await sheets.spreadsheets.values.get({
-      spreadsheetId: process.env.GOOGLE_SHEET_ID,
-      range: "Accepted!A2:H389",
-    });
-
-    const data = [];
-
-    sheetData.data.values.forEach((sD) => {
-      const d = {};
-
-      d.song_title = sD[3];
-      d.suno_username = sD[4];
-      d.song_url = sD[5];
-      d.song_country = sD[7];
-
-      data.push(d);
-    });
+    const jsonData = await fs.readFile(filePath, "utf8");
+    
+    const data = JSON.parse(jsonData);
 
     res.status(200).json(data);
   } catch (err) {
