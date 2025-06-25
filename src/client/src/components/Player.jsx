5186a267e912ee35f5aa481349baa6538be34560
Marco Dal Zovo <marco.dalzovo@tiq.it>
2025-06-25 09:09:06 +0200

    Responsivity fix

--- src/client/src/components/Player.jsx
+++ src/client/src/components/Player.jsx
@@ -13,6 +13,8 @@ function Player() {
   const [totalDuration, setTotalDuration] = useState(0);
   const [currentTime, setCurrentTime] = useState(0);
 
+  const [isFromMobile, setIsFromMobile] = useState(false);
+
   const playingIndex = useRef(0);
   const audioRef = useRef(null);
   const trackWidth = useRef(0);
@@ -27,9 +29,18 @@ function Player() {
       }
     };
 
+    const handleWindowResize = () => {
+      setIsFromMobile(window.innerWidth <= 480);
+    };
+
+    window.addEventListener("resize", handleWindowResize);
+
     if (songs.length === 0) {
       fetchSongs();
+      handleWindowResize();
     }
+
+    return () => window.removeEventListener("resize", handleWindowResize);
   }, []);
 
   useEffect(() => {
@@ -230,10 +241,10 @@ function Player() {
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="icon icon-tabler icons-tabler-outline icon-tabler-volume-off"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="icon icon-tabler icons-tabler-outline icon-tabler-volume-off"
                     >
                       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                       <path d="M15 8a5 5 0 0 1 1.912 4.934m-1.377 2.602a5 5 0 0 1 -.535 .464" />
@@ -249,10 +260,10 @@ function Player() {
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="icon icon-tabler icons-tabler-outline icon-tabler-volume"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="icon icon-tabler icons-tabler-outline icon-tabler-volume"
                     >
                       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                       <path d="M15 8a5 5 0 0 1 0 8" />
@@ -263,39 +274,41 @@ function Player() {
                 </div>
               </div>
               <div className="deck">
-                <div
-                  className={`button change ${isFetching ? "disabled" : ""}`}
-                  onClick={() => {
-                    let t = audioRef.current.currentTime;
+                {!isFromMobile && (
+                  <div
+                    className={`button change ${isFetching ? "disabled" : ""}`}
+                    onClick={() => {
+                      let t = audioRef.current.currentTime;
 
-                    t -= 30;
+                      t -= 30;
 
-                    if (t < 0) {
-                      t = 0;
-                    }
+                      if (t < 0) {
+                        t = 0;
+                      }
 
-                    audioRef.current.currentTime = t;
-                  }}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="icon icon-tabler icons-tabler-outline icon-tabler-rewind-backward-30"
+                      audioRef.current.currentTime = t;
+                    }}
                   >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M19.007 16.466a6 6 0 0 0 -4.007 -10.466h-11" />
-                    <path d="M12 15.5v3a1.5 1.5 0 0 0 3 0v-3a1.5 1.5 0 0 0 -3 0z" />
-                    <path d="M6 14h1.5a1.5 1.5 0 0 1 0 3h-.5h.5a1.5 1.5 0 0 1 0 3h-1.5" />
-                    <path d="M7 9l-3 -3l3 -3" />
-                  </svg>
-                </div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="icon icon-tabler icons-tabler-outline icon-tabler-rewind-backward-30"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                      <path d="M19.007 16.466a6 6 0 0 0 -4.007 -10.466h-11" />
+                      <path d="M12 15.5v3a1.5 1.5 0 0 0 3 0v-3a1.5 1.5 0 0 0 -3 0z" />
+                      <path d="M6 14h1.5a1.5 1.5 0 0 1 0 3h-.5h.5a1.5 1.5 0 0 1 0 3h-1.5" />
+                      <path d="M7 9l-3 -3l3 -3" />
+                    </svg>
+                  </div>
+                )}
                 <div
                   className={`button change ${isFetching ? "disabled" : ""}`}
                   onClick={() => {
@@ -314,7 +327,7 @@ function Player() {
                     height="24"
                     viewBox="0 0 24 24"
                     fill="currentColor"
-                    class="icon icon-tabler icons-tabler-filled icon-tabler-player-skip-back"
+                    className="icon icon-tabler icons-tabler-filled icon-tabler-player-skip-back"
                   >
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <path d="M19.496 4.136l-12 7a1 1 0 0 0 0 1.728l12 7a1 1 0 0 0 1.504 -.864v-14a1 1 0 0 0 -1.504 -.864z" />
@@ -332,7 +345,7 @@ function Player() {
                       height="48"
                       viewBox="0 0 24 24"
                       fill="currentColor"
-                      class="icon icon-tabler icons-tabler-filled icon-tabler-player-pause"
+                      className="icon icon-tabler icons-tabler-filled icon-tabler-player-pause"
                     >
                       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                       <path d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" />
@@ -345,7 +358,7 @@ function Player() {
                       height="48"
                       viewBox="0 0 24 24"
                       fill="currentColor"
-                      class="icon icon-tabler icons-tabler-filled icon-tabler-player-play"
+                      className="icon icon-tabler icons-tabler-filled icon-tabler-player-play"
                     >
                       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                       <path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" />
@@ -370,46 +383,48 @@ function Player() {
                     height="24"
                     viewBox="0 0 24 24"
                     fill="currentColor"
-                    class="icon icon-tabler icons-tabler-filled icon-tabler-player-skip-forward"
+                    className="icon icon-tabler icons-tabler-filled icon-tabler-player-skip-forward"
                   >
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <path d="M3 5v14a1 1 0 0 0 1.504 .864l12 -7a1 1 0 0 0 0 -1.728l-12 -7a1 1 0 0 0 -1.504 .864z" />
                     <path d="M20 4a1 1 0 0 1 .993 .883l.007 .117v14a1 1 0 0 1 -1.993 .117l-.007 -.117v-14a1 1 0 0 1 1 -1z" />
                   </svg>
                 </div>
-                <div
-                  className={`button change ${isFetching ? "disabled" : ""}`}
-                  onClick={() => {
-                    let t = audioRef.current.currentTime;
+                {!isFromMobile && (
+                  <div
+                    className={`button change ${isFetching ? "disabled" : ""}`}
+                    onClick={() => {
+                      let t = audioRef.current.currentTime;
 
-                    t += 30;
+                      t += 30;
 
-                    if (t > audioRef.current.duration) {
-                      t = audioRef.current.duration;
-                    }
+                      if (t > audioRef.current.duration) {
+                        t = audioRef.current.duration;
+                      }
 
-                    audioRef.current.currentTime = t;
-                  }}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="icon icon-tabler icons-tabler-outline icon-tabler-rewind-forward-30"
+                      audioRef.current.currentTime = t;
+                    }}
                   >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M5.007 16.478a6 6 0 0 1 3.993 -10.478h11" />
-                    <path d="M15 15.5v3a1.5 1.5 0 0 0 3 0v-3a1.5 1.5 0 0 0 -3 0z" />
-                    <path d="M17 9l3 -3l-3 -3" />
-                    <path d="M9 14h1.5a1.5 1.5 0 0 1 0 3h-.5h.5a1.5 1.5 0 0 1 0 3h-1.5" />
-                  </svg>
-                </div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="icon icon-tabler icons-tabler-outline icon-tabler-rewind-forward-30"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                      <path d="M5.007 16.478a6 6 0 0 1 3.993 -10.478h11" />
+                      <path d="M15 15.5v3a1.5 1.5 0 0 0 3 0v-3a1.5 1.5 0 0 0 -3 0z" />
+                      <path d="M17 9l3 -3l-3 -3" />
+                      <path d="M9 14h1.5a1.5 1.5 0 0 1 0 3h-.5h.5a1.5 1.5 0 0 1 0 3h-1.5" />
+                    </svg>
+                  </div>
+                )}
                 {song.audio_src && (
                   <audio
                     hidden
